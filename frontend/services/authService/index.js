1c25ece6afc357977f67867820ad08af0a39228e
h4k1 <wilber.garcia@outlook.es>
2023-07-09 14:42:36 -0400

    some changes

--- frontend/services/authService/index.js
+++
@@ -1,7 +0,0 @@
-import api from '@/services/base'
-
-const authService = {
-  signIn: (params) => api.post('/users/sign_in', params)
-}
-
-export default authService
--- frontend/services/base.js
+++ frontend/services/base.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://localhost:5100',
   timeout: 1000,
   withCredentials: true
 })
